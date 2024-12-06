85c1e85de83e952e0aae771f522da671a1210aca
Mehar Channa <bt21cme086@students.vnit.ac.in>
2024-12-07 01:17:39 +0530

    changes

---
+++ server/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const AdminSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['Operator', 'Officer'], default: 'Operator' },
+});
+
+AdminSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
+
+AdminSchema.methods.comparePassword = async function(password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', AdminSchema);
+
+module.exports = User;
