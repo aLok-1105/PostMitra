9d7b32830ebcccc6a0f507f22d4b89dacab5c9bc
grv-vishal <vishalgaurav078@gmail.com>
2024-12-07 00:03:30 +0530

    frontend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import Clerk from './pages/Clerk';
+import Officer from './pages/Officer';
+import Map from './pages/Map';
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="text-center">
       <h1>Hello we are team Synntax</h1>
+
+
+
+      {/* Define routes for different pages*/}
+
+      <Routes>
+        <Route path='/operator' element={<Clerk/>}/>
+        <Route path='/officer' element={<Officer/>}/>
+        <Route path='/map' element={<Map/>}/>
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom'
+import ContextAPI from './components/ContextAPI';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <ContextAPI>
+      <App />
+    </ContextAPI>
+    </BrowserRouter>
+    
   </React.StrictMode>
 );
