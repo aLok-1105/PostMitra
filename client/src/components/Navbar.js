f1f8d5ee8222d6699bdd17c26dcc3d1a6e89a98f
grv-vishal <vishalgaurav078@gmail.com>
2024-12-11 23:20:20 +0530

    home update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,32 +1,20 @@
 import React, { useContext } from 'react'
 import img from '../assets/logo.png'
 import {Link} from 'react-router-dom'
-import { AppContext } from './ContextAPI'
 
 const Navbar = () => {
 
-    const{isLogin}=useContext(AppContext);
-
   return (
     <div className='sticky top-0 z-50 w-full'>
-        <div className='flex px-4 items-center py-3 bg-white shadow-lg justify-between relative h-[60px] w-full'>
-          <div className=' absolute left-[55px] -bottom-[60px] w-[200px] h-[90px] animate-scale-pulse'>
-            <img src={img} alt="India Post" />
-          </div>
+        <div className='flex px-4 items-center py-2 bg-white shadow-lg justify-between w-full'>
+          <Link to="/">
+               <img src={img} alt="India Post" width="70px" height="70px" className='mx-6'/>
+          </Link>
 
           <div className='flex flex-row gap-x-1 justify-center items-center absolute right-6'>
             <Link to="/track-consignment">
-              <p className='cursor-pointer text-xl font-semibold'>Track Consignment</p>
+              <p className='cursor-pointer text-xl font-semibold hover:underline mx-4'>Track Consignment</p>
             </Link>
-
-            {!isLogin && 
-            <Link to="/login">
-             <button className=" text-black font-bold py-1 px-4 text-xl  hover:bg-white transition-all duration-400
-            hover:text-blue-700">
-                Log In
-             </button>
-            </Link>
-            }
           </div>
         </div>
       
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,23 +5,15 @@ import mix from '../assets/img.png'
 
 
 const Home = () => {
-    const headingText = "Post  Mitra";
+
   return (
-    <div className='flex w-[100%] h-[100%] justify-center items-center text-2xl'>
-      <div className='flex flex-row justify-center items-center gap-2'>
-        <div className='w-1/2 flex flex-col justify-center items-start p-16'>
-            <h2 className="text-5xl text-red-600 font-extrabold flex gap-1 my-6">
-              {headingText.split("").map((char, index) => (
-                <span
-                 key={index}
-                 className="inline-block animate-fade-bounce"
-                 style={{ animationDelay: `${index * 0.2}s` }}
-                >
-                 {char}
-                </span>
-              ))}
+    <div className='w-[100%] h-[100%] flex flex-col justify-center'>
+      <div className='flex flex-row justify-center items-center gap-2 '>
+        <div className='w-1/2 flex flex-col justify-center items-start px-16'>
+            <h2 className="text-5xl text-red-600 font-extrabold flex gap-1 my-4">
+              Post Mitra
             </h2>
-            <p className='text-gray-600 mt-2'>
+            <p className='text-gray-600 mt-2 text-xl'>
                 Welcome to Post Mitra, your trusted partner for seamless and reliable parcel deliveries with India Speed Post. 
                 Whether it's documents, packages, or special consignments, we ensure timely delivery across the nation. 
             </p>
@@ -30,15 +22,17 @@ const Home = () => {
               <button className=' my-5 py-2 px-3 text-2xl text-white bg-red-600 rounded-md shadow-xl font-semibold hover:scale-110 transition-all duration-500'>Get Started</button>
             </Link>
         </div>
-        <div className='w-1/2 flex flex-col justify-center items-center gap-y-12 '>
-            <div>
-            <img src={train} alt="" width="550px" height="400px" loading='lazy' className=" absolute left-[100px] top-[80px] animate-move-horizontal"/>
-            </div>
+        
+        <div className='w-1/2 flex flex-col justify-center items-center '>
             <div>
                 <img src={mix} alt="" width="400px" height="400px" loading='lazy' className='' />
             </div>
         </div>
-            
+
+      </div>
+
+      <div>
+        <img src={train} alt="" width="550px" height="400px" loading='lazy' className=" animate-move-horizontal"/>
       </div>
     </div>
   )
